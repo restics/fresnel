594d986e30355a7177ad8facf120c119d5395ee3
LabUser <mjliu1@asu.edu>
2025-06-09 16:47:28 -0400

    update settingspane

--- Assets/Scripts/SettingsPanelUI.cs
+++ Assets/Scripts/SettingsPanelUI.cs
@@ -31,7 +31,7 @@ public class SettingsPanelUI : MonoBehaviour
     }
 
 
-    public void OnOffsetSliderUpdate(bool isLeft){
+    public void OnOffsetSliderUpdate(){
         var slider = GetComponent<Slider>();
 
         var streamManager = FindFirstObjectByType<StreamManager>();
@@ -39,9 +39,34 @@ public class SettingsPanelUI : MonoBehaviour
             Debug.LogError("Slider or stream manager not found");
             return; 
         }
-        var mappedOffset = slider.value * 0.1f - 0.05f;
-        streamManager.UpdateOffset(mappedOffset, isLeft);
+        var mappedOffset = Mathf.Lerp(-0.05f, 0.05f, slider.value);
+        streamManager.UpdateOffset(mappedOffset);
 
-        Debug.Log($" Offset updated to {mappedOffset} for {(isLeft ? "left" : "right")} eye");
+        Debug.Log($" Offset updated to {mappedOffset}");
+    }
+
+
+    public void OnScaleSliderUpdate(){
+        var slider = GetComponent<Slider>();
+
+        var streamManager = FindFirstObjectByType<StreamManager>();
+        if (slider == null || streamManager == null){ 
+            Debug.LogError("Slider or stream manager not found");
+            return; 
+        }
+        var mappedScale = Mathf.Lerp(0.1f, 1.0f, slider.value);
+        streamManager.transform.localScale = new Vector3(mappedScale*160, mappedScale*90, 0.1f);
+
+        Debug.Log($" Scale updated to {mappedScale}");
+    }
+    public void OnStereoModeUpdate(){
+        var toggle = GetComponent<Toggle>();
+        var streamManager = FindFirstObjectByType<StreamManager>();
+        if (toggle == null || streamManager == null){ 
+            Debug.LogError("Toggle or stream manager not found");
+            return; 
+        }
+        streamManager.UpdateStereoMode(toggle.isOn);
+        Debug.Log($" Stereo mode updated to {toggle.isOn}");
     }
 }
